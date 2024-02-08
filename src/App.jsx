fc82d5e390d5a9c7cddf3269691612e5a2580ef4
danie odukwe <kingdanny295@gmail.com>
2024-02-08 14:22:17 +0100

    made a more dynamic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import Home from "./pages/Home"
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom"
 import Users from "./pages/Users"
 import Products from "./pages/Products"
-
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+import SideBar from "./components/SideBar"
 const App = () => {
+
+const Layout = () => {
+  return (
+    <div className="main">
+      <Navbar />
+      <div className="flex">
+        <SideBar />
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+    )
+  }
 
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: <Home />
-    },
-    {
-      path: 'users',
-      element: <Users />
-    },
-    {
-      path: '/products',
-      element: <Products />
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path:"/",
+          element:<Home />
+        },
+        {
+          path: '/user',
+          element:<Users />
+        },
+        {
+          path: '/products',
+          element: <Products />
+        }
+      ]
     },
   ])
 
