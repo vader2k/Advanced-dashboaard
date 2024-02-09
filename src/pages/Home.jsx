242db88c7ff3e15ac7b856ac3d0ccdba6e86521a
danie odukwe <kingdanny295@gmail.com>
2024-02-09 13:22:20 +0100

    working on bar chart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import TopBox from "../components/TopBox"
 import ChartBox from "../components/ChartBox"
+import BarChart from "../components/BarChart"
 
 const Home = () => {
   return (
@@ -7,34 +8,67 @@ const Home = () => {
       <div className="border border-softBg p-5 rounded-xl min-h-[180px] h-auto row-span-3">
         <TopBox />
       </div>
+
       <div className="border border-softBg p-5 rounded-xl min-h-[180px] h-auto">
         <ChartBox
           title={"Total Users"} 
           number={11.238}
           link={"View all"}
           percentage={"45%"}
-          varient={"text-blue-500 text-[2rem]"}
+          varient={"text-gray-500 text-[2rem]"}
           col={"text-green-500"}
+          color={'gray'}
         />
       </div>
+
       <div className="border border-softBg p-5 rounded-xl min-h-[180px] h-auto">
-        <ChartBox />
+        <ChartBox 
+          title={"Total Products"}
+          number={238}
+          link={"View all"}
+          percentage={"21%"}
+          varient={"text-blue-200 text-[2rem]"}
+          col={"text-green-500"}
+          color={'skyblue'}
+        />
       </div>
+
       <div className="border border-softBg p-5 rounded-xl min-h-[180px] h-auto row-span-3">
         box4
       </div>
+
       <div className="border border-softBg p-5 rounded-xl min-h-[180px] h-auto">
-        <ChartBox />
+        <ChartBox
+         title={"Total Revenue"}
+         number={"$56.432"}
+         link={"View all"}
+         percentage={"-12%"}
+         varient={"text-teal-200 text-[2rem]"}
+         col={"text-red-500"}
+         color={'teal'}
+        />
       </div>
+      
       <div className="border border-softBg p-5 rounded-xl min-h-[180px] h-auto">
-        <ChartBox />
+        <ChartBox 
+          title={"Total Ratio"}
+          number={2.6}
+          link={"View all"}
+          percentage={"12%"}
+          varient={"text-yellow-200 text-[2rem]"}
+          col={"text-green-500"}
+          color={'gold'}
+        />
       </div>
+
       <div className="border border-softBg p-5 rounded-xl min-h-[180px] h-auto row-span-2 col-span-2">
         box7
       </div>
+
       <div className="border border-softBg p-5 rounded-xl min-h-[180px] h-auto">
-        box8
+        <BarChart />
       </div>
+
       <div className="border border-softBg p-5 rounded-xl min-h-[180px] h-auto">
         box9
       </div>
