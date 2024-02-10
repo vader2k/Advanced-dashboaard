3772ea31164b5b3c2e47f54f3e384cb98bed35e8
danie odukwe <kingdanny295@gmail.com>
2024-02-10 10:06:55 +0100

    fixed issue with div color

--- src/components/PieChartBox.jsx
+++ src/components/PieChartBox.jsx
@@ -1,10 +1,10 @@
 import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts"
 
 const data = [
-    { name: "Mobile", value:400, color: "#0088FE", text: "blue-500" },
-    { name: "Desktop", value:300, color: "#00C49F" , text: "green-300" },
-    { name: "Laptop", value:300, color: "#FFBB28", text: "yellow-300" },
-    { name: "Tablet", value:200, color: "#FF8042", text: "orange-300" },
+    { name: "Mobile", value:400, color: "#0088FE" },
+    { name: "Desktop", value:300, color: "#00C49F" },
+    { name: "Laptop", value:300, color: "#FFBB28" },
+    { name: "Tablet", value:200, color: "#FF8042" },
 ]
 const PieChartBox = () => {
   return (
@@ -37,7 +37,7 @@ const PieChartBox = () => {
             {data.map(item => (
                 <div key={item.name} className="flex flex-col gap-5 items-center">
                     <div className="flex gap-3 items-center">
-                        <div className={`bg-${item.text} w-[10px] h-[10px] rounded-[50%]`}/>
+                        <div className="w-[10px] h-[10px] rounded-[50%]" style={{ background: `${item.color}`}}/>
                         <span>{item.name}</span>
                     </div>
                     <span>{item.value}</span>
